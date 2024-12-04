ddc59c01c5c12c946aadcb314a96cdd6ebfb7768
barakk123 <barakk123@gmail.com>
2024-12-05 00:58:43 +0200

    Initial commit of MiniTools library

---
+++ minitools.h
@@ -0,0 +1,9 @@
+#ifndef MINITOOLS_H
+#define MINITOOLS_H
+
+// Function to prompt the user for a valid integer input and return the input value.
+// This function ensures the input is a single integer with no extra characters (e.g., spaces or other numbers).
+// It will repeatedly prompt the user until valid input is received.
+int get_valid_input(void);
+
+#endif
